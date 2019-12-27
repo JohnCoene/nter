040bda3b3f74fc62b496d170d66cc3188e287744
John Coene <jcoenep@gmail.com>
2019-12-27 10:30:45 +0100

    remove key argument, useless

--- R/core.R
+++ R/core.R
@@ -5,12 +5,10 @@
 #' @param id Id of button.
 #' @param input Active input that triggers, useful if you have 
 #' more than one button triggered by an enter key.
-#' @param key The key that triggers the button, defaults to \code{keyup}
-#' which is the enter key.
 #' 
 #' @import shiny 
 #' @export
-nter <- function(id, input = "window", key = "keyup") {
+nter <- function(id, input = "window") {
 
   input <- if(!is.null(input)){
     input <- paste0("document.getElementById('", input, "')")
@@ -18,7 +16,7 @@ nter <- function(id, input = "window", key = "keyup") {
 
   tags$script(
     paste0(
-      input, '.addEventListener("', key, '", function(event) {
+      input, '.addEventListener("keyup", function(event) {
         if (event.keyCode === 13) {
           document.getElementById("', id, '").click();
         }
